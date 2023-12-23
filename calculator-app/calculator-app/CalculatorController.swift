73a0e81593ee8e01088d915b27a89795b19bed76
Ayberk Bilgiç <lewidvailed@outlook.com>
2023-12-23 15:49:42 +0300

    collection view update

--- calculator-app/calculator-app/CalculatorController.swift
+++ calculator-app/calculator-app/CalculatorController.swift
@@ -11,9 +11,12 @@ class CalculatorController: UIViewController {
 
     // MARK: - UI Components
     private let collectionView: UICollectionView = {
+        
+        // defines the layout and determines the scroll direction
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         
+        // Registers the information type for the collection view.
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.backgroundColor = .black
         collectionView.register(HeaderCell.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HeaderCell.identifier)
@@ -28,13 +31,17 @@ class CalculatorController: UIViewController {
         self.view.backgroundColor = .blue
         self.setupUI()
         
+        // delegate and datasource protocols are defined
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
     }
 
     // MARK: - UI Setup
     private func setupUI() {
+        //Is where view objects are added
         self.view.addSubview(self.collectionView)
+        
+        //It is important for auto layout usage
         self.collectionView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
@@ -48,6 +55,8 @@ class CalculatorController: UIViewController {
 
 // MARK: - Extension
 extension CalculatorController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    // MARK: - Buttons
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 6
     }
@@ -56,9 +65,13 @@ extension CalculatorController: UICollectionViewDelegate, UICollectionViewDataSo
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ButtonCell.identifier, for: indexPath) as? ButtonCell else {
             fatalError("Failed to dequeue ButtonCell in CC")
         }
+        cell.backgroundColor = .blue
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.size.width/5, height: view.frame.size.height/5)
+    }
         
 }
 
